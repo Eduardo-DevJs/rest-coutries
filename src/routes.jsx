15e020f7e527f61b07fe91ffe7dbaf0e0b99a924
Eduardo-DevJs <dudev2001@gmail.com>
2024-01-12 15:56:35 -0300

    Alterano no nome da pasta

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
-import Layout from "./components/Layouts/Layout";
+import Layout from "./components/layouts/Layout";
 import Detalhes from "./pages/Detalhes/Detalhes";
 
 export const router = createBrowserRouter([
